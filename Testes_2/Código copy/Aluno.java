5fd378bf67a8192bfa74b59b3be7f434a5e546b5
PHPGomes <pedrohpgomes@gmail.com>
2023-07-01 19:48:35 -0300

    .
    .

--- Testes_2/Código copy/Aluno.java
+++ Testes_2/Código copy/Aluno.java
@@ -1,11 +1,9 @@
 import java.awt.Image;
 import javax.swing.ImageIcon;
 
-/**
- * Representa os veiculos da simulacao.
- * @author David J. Barnes and Michael Kolling and Luiz Merschmann
- */
+
 public class Aluno extends Item{
+
     private int tempoChegada;
     private int tempoEntrada;
 
