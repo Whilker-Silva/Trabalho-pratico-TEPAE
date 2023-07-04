7e2a83a2b1e4eda7934b709b2acdc619a81fef87
Whilker Henrique dos Santos Silva <77589533+Whilker-Silva@users.noreply.github.com>
2023-07-04 11:01:07 -0300

    dividindo classes item

--- Código/Aluno.java
+++ Código/Aluno.java
@@ -2,34 +2,25 @@ import javax.swing.ImageIcon;
 
 /**
  * Representa os veiculos da simulacao.
+ * 
  * @author David J. Barnes and Michael Kolling and Luiz Merschmann
  */
-public class Aluno extends Item{
-    private int tempoChegada;
+public class Aluno extends ItemDinamico {
     private int tempoEntrada;
 
-    public Aluno(Localizacao localizacao, int tempoChegada, int tempoEntrada){
-        super(localizacao);
+    public Aluno(Localizacao localizacaoAtual, Localizacao localizacaoDestino, int tempoEntrada) {
+        super(localizacaoAtual, localizacaoDestino);
         setImagem(new ImageIcon(getClass().getResource("Imagens/Pessoa.png")).getImage());
-        this.tempoChegada = tempoChegada;
         this.tempoEntrada = tempoEntrada;
     }
 
-    public String toString(){
-        return "\nTempo Chegada:" + tempoChegada + " Tempo Entrada:" + tempoEntrada;
-    }
-
-    public Localizacao getProximalocalizao(){
+    public Localizacao getProximalocalizao() {
         Localizacao destino = getLocalizacaoDestino();
-            return (destino != null) ? getLocalizacaoAtual().proximaLocalizacao(getLocalizacaoDestino()) : null;
-    }
-
-    public int getTempoChegada() {
-        return tempoChegada;
+        return (destino != null) ? getLocalizacaoAtual().proximaLocalizacao(getLocalizacaoDestino()) : null;
     }
 
     public int getTempoEntrada() {
         return tempoEntrada;
     }
-    
+
 }
