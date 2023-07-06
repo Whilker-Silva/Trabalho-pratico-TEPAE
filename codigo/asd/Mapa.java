f20cebac4ae7593567472ef3cb2d921841c7ed92
PHPGomes <pedrohpgomes@gmail.com>
2023-07-06 09:03:08 -0300

    .
    .

---
+++ codigo/asd/Mapa.java
@@ -0,0 +1,47 @@
+/**
+ * Representa um mapa com todos os itens que participam da simulacao
+ * @author David J. Barnes and Michael Kolling and Luiz Merschmann
+ */
+public class Mapa {
+    private Item[][] itens;
+    private int largura;
+    private int altura;    
+    
+    /**
+     * Cria mapa para alocar itens da simulacao.
+     * @param largura: largura da área de simulacao.
+     * @param altura: altura da área de simulação.
+     */
+    public Mapa(int largura, int altura) {
+        this.largura = largura;
+        this.altura = altura;
+        itens = new Item[altura][largura];
+    }
+   
+    
+    public void adicionarItem(Item v){
+        itens[v.getLocalizacaoAtual().getX()][v.getLocalizacaoAtual().getY()] = v;
+    }
+    
+    public void removerItem(Item v){
+        itens[v.getLocalizacaoAtual().getX()][v.getLocalizacaoAtual().getY()] = null;
+    }
+    
+    public void atualizarMapa(Item v){
+        removerItem(v);
+        adicionarItem(v);
+    }
+    
+    public Item getItem(int x, int y){
+        return itens[x][y];
+    }
+
+    public int getLargura() {
+        return largura;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+    
+}
---
+++ codigo/asd/Principal.java
@@ -0,0 +1,13 @@
+/**
+ * Classe que contem o método main, responsável apenas por instanciar o objeto
+ * Simulacao e chamar o método executar a simulação *
+ * 
+ * @author Luiz Merschmann
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        Simulacao simulador = new Simulacao();
+        simulador.executarSimulacao(100);
+    }
+}
