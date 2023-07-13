fab579dae5a7a20973f04766e9ed65083e84845f
PHPGomes <pedrohpgomes@gmail.com>
2023-07-12 21:37:35 -0300

    .
    .

--- br/ufla/gac111/grupo3/PontoParada.java
+++ br/ufla/gac111/grupo3/PontoParada.java
@@ -34,7 +34,7 @@ public class PontoParada extends Item {
         return getPrimeiroAluno().getEmbarcou();
     }
 
-    private Aluno getPrimeiroAluno() {
+    public Aluno getPrimeiroAluno() {
         return filaAlunos.get(0);
     }
 
