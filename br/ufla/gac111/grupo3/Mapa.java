d3b4c19e5f4ef4f4e49e9fb1dbfe2ca2e5d6256e
Whilker Henrique dos Santos Silva <77589533+Whilker-Silva@users.noreply.github.com>
2023-07-13 09:57:05 -0300

    .

--- br/ufla/gac111/grupo3/Mapa.java
+++ br/ufla/gac111/grupo3/Mapa.java
@@ -1,10 +1,14 @@
 package br.ufla.gac111.grupo3;
 
 /**
- * Representa um mapa com todos os itens que participam da simulacao.
+ * Representa o mapa da com todos os itens que participam da simulacao.
  * <p>
  * A criação do mapa é realizada instanciando uma matriz de Itens cujas
  * dimensões são informadas por paramentro
+ * <p>
+ * As posições da matriz são inciadas por padrão com null, dessa forma para
+ * adicionar um item no mapa, basta adicionar um obejto na posição desejada na
+ * matriz
  * 
  * @author David J. Barnes and Michael Kolling and Luiz Merschmann
  * @author Whilker Henrique Dos Santos Silva
@@ -28,27 +32,53 @@ public class Mapa {
         itens = new Item[altura][largura];
     }
 
+    /**
+     * Método para adionar um obejto do tipo Item no Mapa
+     * 
+     * @param i - Item que deseja adiconar
+     */
     public void adicionarItem(Item i) {
         itens[i.getLocalizacaoAtual().getX()][i.getLocalizacaoAtual().getY()] = i;
     }
 
+    /**
+     * Método para remover um obejto do tipo Item no Mapa
+     * 
+     * @param i - item que deseja remover
+     */
     public void removerItem(Item i) {
         itens[i.getLocalizacaoAtual().getX()][i.getLocalizacaoAtual().getY()] = null;
     }
 
+    /**
+     * Método responsavel por atualizar os itens do Mapa
+     * 
+     * @param i
+     */
     public void atualizarMapa(Item i) {
         removerItem(i);
         adicionarItem(i);
     }
 
+    /**
+     * @param x - Posição em ralção ao eixo y
+     * @param y - Posição em ralção ao eixo x
+     * @return Item da posição informada por paramentro
+     */
     public Item getItem(int x, int y) {
         return itens[x][y];
     }
 
+    /**
+     * @return Largura do mapa: int
+     */
     public int getLargura() {
         return largura;
     }
 
+    /**
+     * @return Altura do mapa: int
+     */
     public int getAltura() {
         return altura;
     }
