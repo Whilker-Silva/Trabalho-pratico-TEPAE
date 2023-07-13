0835d6437fb6c67f37172b3faef68faea0b08737
matiashteixeira <mateus.teixeira1@estudante.ufla.br>
2023-07-13 13:28:20 -0300

    Aluno comentado e corrigido

--- br/ufla/gac111/grupo3/Aluno.java
+++ br/ufla/gac111/grupo3/Aluno.java
@@ -3,25 +3,29 @@ package br.ufla.gac111.grupo3;
 import javax.swing.ImageIcon;
 
 /**
- * Classe que representa os alunos da simulação.
+ * Representa os alunos da simulação.
  * <p>
- * Na simulacao aluno é considerado um item que se move, dessa forma a classe
- * Aluno herda de itemDinamico que herda de item.
+ * Na simulacão, aluno é considerado um item que se move, dessa forma a classe
+ * Aluno herda de itemDinamico, que herda de item.
  * 
  * @author David J. Barnes and Michael Kolling and Luiz Merschmann
  * @author Whilker Henrique Dos Santos Silva
  */
+
 public class Aluno extends ItemDinamico {
     private int tempoEntrada;
     private boolean embarcou;
 
     /**
      * Construtor de objetos do tipo Aluno
-     * 
+     * <p>
+     * Atribui uma localizacao para os atributos localizacaoAtual e para localizacaoDestino.
+     * Além disso define o tempo de entrada do aluno.
      * @param localizacaoAtual
      * @param localizacaoDestino
      * @param tempoEntrada
      */
+
     public Aluno(Localizacao localizacaoAtual, Localizacao localizacaoDestino, int tempoEntrada) {
         super(localizacaoAtual, localizacaoDestino);
         setImagem(new ImageIcon(getClass().getResource("Imagens/Pessoa2.png")).getImage());
@@ -29,29 +33,27 @@ public class Aluno extends ItemDinamico {
     }
 
     /**
-     * Método que retorna o tempo em que o aluno irá gastar para embarcar no mamute
-     * 
-     * @return tempoEntrada: int
+     * Retorna o tempo que o aluno irá gastar para embarcar no mamute.
+     * @return int - tempoChegada
      */
+
     public int getTempoEntrada() {
         return tempoEntrada;
     }
 
     /**
-     * 
-     * O atributo embarcou é inicializado por padrão como FALSE
-     * <p>
-     * Dessa forma este método é responsável por mudar o estado para TRUE
+     * Altera o estado inicial do atributo embarcou (FALSE) para (TRUE).
      */
+
     public void setEmbarcou() {
         embarcou = true;
     }
 
     /**
-     * Retorna se o aluno já embarcou no mamute
-     * 
-     * @return embarcou: boolean
+     * Retorna se o aluno já embarcou no mamute.
+     * @return boolean - embarcou
      */
+    
     public boolean getEmbarcou() {
         return embarcou;
     }
