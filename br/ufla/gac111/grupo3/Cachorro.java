dc1f407820e56ef40f908efae16f8a1e5d9c1ddc
Victor Hugo Daia Lorenzato <113929927+Victor-Lorenzato@users.noreply.github.com>
2023-07-13 14:54:32 -0300

    simulacao-comentada

--- br/ufla/gac111/grupo3/Cachorro.java
+++ br/ufla/gac111/grupo3/Cachorro.java
@@ -2,43 +2,45 @@ package br.ufla.gac111.grupo3;
 
 import javax.swing.ImageIcon;
 
-
 /**
- * Cachorro é uma classe responsavel pela criação e movimentaçao do objeto Cachorro
+ * Cachorro é uma classe responsavel pela criação e movimentaçao do objeto
+ * Cachorro
+ * 
  * @author Pedro Henrique Pigozzi Gomes
  */
 
 public class Cachorro extends ItemDinamico implements MovimentacaoPercurso {
-    
 
-    
     /**
      * Construtor de objetos do tipo Cachorro
      * <p>
-     * Recebe por parametro duas variaveis do tipo Localizacao e as atribui as variáveis 
+     * Recebe por parametro duas variaveis do tipo Localizacao e as atribui as
+     * variáveis
      * localizacaoAtual e a localizacaoDestino.
-     * @param localizacao Localizacao que será atribuida a variável localizacaoAtual
-     * @param localizacaoDestino Localizacao que será atribuida a variável localizacaoDestino
+     * 
+     * @param localizacao        Localizacao que será atribuida a variável
+     *                           localizacaoAtual
+     * @param localizacaoDestino Localizacao que será atribuida a variável
+     *                           localizacaoDestino
      */
 
-    public Cachorro(Localizacao localizacao, Localizacao localizacaoDestino){
-        super(localizacao,localizacaoDestino);
+    public Cachorro(Localizacao localizacao, Localizacao localizacaoDestino) {
+        super(localizacao, localizacaoDestino);
         setImagem(new ImageIcon(getClass().getResource("Imagens/Cachorro.png")).getImage());
     }
-
-
 
     /**
      * Recebe por parametros duas variaveis do tipo Localizacao
      * e realiza a ação de movimentar o Objeto pelo mapa,
-     * logo depois realiza a função trocarDestino que, caso o Cachorro tenha chegado em seu destino,
+     * logo depois realiza a função trocarDestino que, caso o Cachorro tenha chegado
+     * em seu destino,
      * ele troca o destino do ponto1 para o ponto2
      */
 
     @Override
-    public void realizarPercurso(Localizacao localizacaoPonto1, Localizacao localizacaoPonto2){
+    public void realizarPercurso(Localizacao localizacaoPonto1, Localizacao localizacaoPonto2) {
         executarAcao();
         trocaDestino(localizacaoPonto1, localizacaoPonto2);
     }
 
-} 
+}
--- br/ufla/gac111/grupo3/Simulacao.java
+++ br/ufla/gac111/grupo3/Simulacao.java
@@ -13,6 +13,7 @@ import java.util.Random;
  * @author David J. Barnes and Michael Kolling and Luiz Merschmann
  * @author
  */
+
 public class Simulacao {
 
     private JanelaSimulacao janelaSimulacao;
@@ -25,12 +26,23 @@ public class Simulacao {
 
     private static Simulacao instanciaUnica;
 
+    /**
+     * -/-/-/-/-
+     */
     private Simulacao() {
         mapa = new Mapa(22, 22);
         criaItensSimulacao();
-        criaItensDecoracao(pontoEmbarque,pontoDesembarque);
+        criaItensDecoracao(pontoEmbarque, pontoDesembarque);
         janelaSimulacao = new JanelaSimulacao(mapa);
     }
+
+    /**
+     * Implementa o padrão de projeto Singleton para garantir que apenas
+     * uma instância da classe Simulacao seja criada.
+     * <p>
+     * 
+     * @return Única instância da classe Simulacao caso já tenha sido criada.
+     */
 
     public static Simulacao getInstance() {
         if (instanciaUnica == null) {
@@ -39,6 +51,17 @@ public class Simulacao {
         return instanciaUnica;
     }
 
+    /**
+     * Inicia a execução da simulação
+     * <p>
+     * Chama o método executarAcao() da janela de simulação para exbir a janela.
+     * Inicia um loop que executa o excutarumPasso() para cada tempo de simulação.
+     * O número de etapas é determinado pelo parâmetro tempoSimulacao.
+     * 
+     * @param tempoSimulacao
+     * @param qtdAlunosMax
+     */
+
     public void executarSimulacao(int tempoSimulacao, int qtdAlunosMax) {
         janelaSimulacao.executarAcao();
         for (int i = 0; i < tempoSimulacao; i++) {
@@ -46,25 +69,47 @@ public class Simulacao {
         }
     }
 
+    /**
+     * Representa um passo do aluno na simulação
+     * <p>
+     * Cria aluno nos pontos de embarque e desembarque.
+     * Embarca aluno e movimenta a fila.
+     * 
+     * @param tempoSimulacao
+     * @param qtdAlunosMax
+     */
+
     private void executarUmPasso(int tempoSimulacao, int qtdAlunosMax) {
-        criarAlunos(tempoSimulacao, pontoEmbarque,qtdAlunosMax);
-        criarAlunos(tempoSimulacao, pontoDesembarque,qtdAlunosMax);
+        criarAlunos(tempoSimulacao, pontoEmbarque, qtdAlunosMax);
+        criarAlunos(tempoSimulacao, pontoDesembarque, qtdAlunosMax);
 
         embarcarAluno(tempoSimulacao, pontoEmbarque);
         embarcarAluno(tempoSimulacao, pontoDesembarque);
 
-        movimentaFila(pontoEmbarque, tempoSimulacao);        
+        movimentaFila(pontoEmbarque, tempoSimulacao);
         movimentaFila(pontoDesembarque, tempoSimulacao);
     }
+
+    /**
+     * Cria alunos nos pontos de embarque e desembarque
+     * <p>
+     * Gera um número aleatório de alunos.
+     * Se o ponto de parada não estiver cheio -/-/-/-/-/
+     * 
+     * @param tempoSimulacao
+     * @param pontoParada
+     * @param qtdAlunosMax
+     */
 
     private void criarAlunos(int tempoSimulacao, PontoParada pontoParada, int qtdAlunosMax) {
         Random e = new Random();
         int qtdAlunos = e.nextInt(qtdAlunosMax);
 
-        if (!pontoParada.estaCheia() && tempoSimulacao%3 == 0) {
+        if (!pontoParada.estaCheia() && tempoSimulacao % 3 == 0) {
             for (int i = 0; i < qtdAlunos; i++) {
                 int tempoEntrada = e.nextInt(2) + 1;
-                Localizacao inicioFila = new Localizacao(pontoParada.getLocalizacaoAtual().getX() + 1, pontoParada.getLocalizacaoAtual().getY());
+                Localizacao inicioFila = new Localizacao(pontoParada.getLocalizacaoAtual().getX() + 1,
+                        pontoParada.getLocalizacaoAtual().getY());
                 Aluno aluno = new Aluno(pontoParada.posicaoLivre(), inicioFila, tempoEntrada);
                 pontoParada.adicionarAluno(aluno);
                 atualizarPontoParada(pontoParada);
@@ -72,6 +117,19 @@ public class Simulacao {
             }
         }
     }
+
+    /**
+     * Realiza o embarque de alunos no mamute
+     * <p>
+     * Verifica se o mamute está disponível para embarque e se o ponto de parada não
+     * está vazio.
+     * Se as condições forem atendidas, verifica se o aluno embarcou.
+     * Se o aluno não embarcou, define o tempo para a próxima entrada.
+     * Se o aluno embarcou, remove o primeiro aluno da fila.
+     * 
+     * @param tempoSimulacao
+     * @param pontoParada
+     */
 
     private void embarcarAluno(int tempoSimulacao, PontoParada pontoParada) {
         if (mamute.estaDisponivel(tempoSimulacao, pontoParada) && !pontoParada.estaVazia()) {
@@ -93,6 +151,16 @@ public class Simulacao {
         }
     }
 
+    /**
+     * Movimenta a fila nos pontos
+     * <p>
+     * Se o ponto de parada não estiver vazio, atualiza a fila para cada aluno.
+     * Se o método atualizaFila() retornar true, o aluno foi movido.
+     * 
+     * @param pontoParada
+     * @param tempoSimulacao
+     */
+
     private void movimentaFila(PontoParada pontoParada, int tempoSimulacao) {
         if (!pontoParada.estaVazia()) {
             boolean aux = true;
@@ -101,13 +169,19 @@ public class Simulacao {
                 for (int i = 0; i <= pontoParada.tamanhoFila() - 1; i++) {
                     if (pontoParada.atualizaFila(i)) {
                         aux = true;
-                        movimentaMamute(pontoEmbarque, pontoDesembarque); 
+                        movimentaMamute(pontoEmbarque, pontoDesembarque);
                         moverCachorro();
                     }
                 }
             }
         }
     }
+
+    /**
+     * Pausa a execução do programa por alguns milissegundos
+     * 
+     * @param milisegundos
+     */
 
     private void esperar(int milisegundos) {
         try {
@@ -117,6 +191,16 @@ public class Simulacao {
         }
     }
 
+    /**
+     * Movimenta o mamute entre os pontos de parada
+     * <p>
+     * Chama o método realizarPercurso(), espera por um tempo e executa na janela de
+     * simulação.
+     * 
+     * @param ponto1
+     * @param ponto2
+     */
+
     private void movimentaMamute(PontoParada ponto1, PontoParada ponto2) {
         mamute.realizarPercurso(ponto1.getLocalizacaoAtual(), ponto2.getLocalizacaoAtual());
         esperar(50);
@@ -124,51 +208,75 @@ public class Simulacao {
         esperar(50);
     }
 
+    /**
+     * Atualiza a posição dos alunos no ponto de parada no mapa
+     * <p>
+     * 
+     * @param pontoParada
+     */
+
     private void atualizarPontoParada(PontoParada pontoParada) {
         for (Aluno a : pontoParada.getFila())
             mapa.atualizarMapa(a);
     }
 
-    private void moverCachorro(){
+    /**
+     * Movimenta o cachorro entr dois pontos
+     * <p>
+     * Chama o método realizarPercurso para excutar a ação de movimentá-lo.
+     */
+    private void moverCachorro() {
         cachorro.realizarPercurso(new Localizacao(15, 2), new Localizacao(4, 2));
     }
 
-    private void criaItensDecoracao(PontoParada pontoEsquerda, PontoParada pontoDireita){
+    /**
+     * Cria itens de decoração entre os dois pontos de parada
+     * <p>
+     * Cria um cachorro e uma sequência de árvores e casas em posições específcas.
+     * 
+     * @param pontoEsquerda
+     * @param pontoDireita
+     */
+
+    private void criaItensDecoracao(PontoParada pontoEsquerda, PontoParada pontoDireita) {
         decoracao = new ArrayList<Item>();
         int inicio = 5;
         int y = 4;
 
-        cachorro = new Cachorro(new Localizacao(20, y-2), new Localizacao(4, y-2));
+        cachorro = new Cachorro(new Localizacao(20, y - 2), new Localizacao(4, y - 2));
         mapa.adicionarItem(cachorro);
 
         for (int i = 0; i < 2; i++) {
-            decoracao.add(new Decoracao(new Localizacao((inicio+0)+6*i, y-1),"Imagens/Arvore1.png"));
-            decoracao.add(new Decoracao(new Localizacao((inicio+1)+6*i, y),"Imagens/Casa1.png"));
-            decoracao.add(new Decoracao(new Localizacao((inicio+2)+6*i, y),"Imagens/Casa2.png"));
-            decoracao.add(new Decoracao(new Localizacao((inicio+3)+6*i, y),"Imagens/Casa3.png"));
-            decoracao.add(new Decoracao(new Localizacao((inicio+4)+6*i, y-1),"Imagens/Arvore2.png"));
+            decoracao.add(new Decoracao(new Localizacao((inicio + 0) + 6 * i, y - 1), "Imagens/Arvore1.png"));
+            decoracao.add(new Decoracao(new Localizacao((inicio + 1) + 6 * i, y), "Imagens/Casa1.png"));
+            decoracao.add(new Decoracao(new Localizacao((inicio + 2) + 6 * i, y), "Imagens/Casa2.png"));
+            decoracao.add(new Decoracao(new Localizacao((inicio + 3) + 6 * i, y), "Imagens/Casa3.png"));
+            decoracao.add(new Decoracao(new Localizacao((inicio + 4) + 6 * i, y - 1), "Imagens/Arvore2.png"));
 
         }
 
-        for (int i = pontoEsquerda.getLocalizacaoAtual().getX()+2; i <= pontoDireita.getLocalizacaoAtual().getX()-1; i++) {
-            decoracao.add(new Decoracao(new Localizacao(i, pontoDireita.getLocalizacaoAtual().getY()), "Imagens/Asfalto.png"));
+        for (int i = pontoEsquerda.getLocalizacaoAtual().getX() + 2; i <= pontoDireita.getLocalizacaoAtual().getX()
+                - 1; i++) {
+            decoracao.add(new Decoracao(new Localizacao(i, pontoDireita.getLocalizacaoAtual().getY()),
+                    "Imagens/Asfalto.png"));
         }
 
         for (Item i : decoracao)
             mapa.adicionarItem(i);
     }
 
-    private void criaItensSimulacao(){
+    /**
+     * -/-/-/-/-/-
+     */
+    private void criaItensSimulacao() {
         pontoEmbarque = new PontoParada(new Localizacao(1, 1));
         mapa.adicionarItem(pontoEmbarque);
-
 
         pontoDesembarque = new PontoParada(new Localizacao(18, 1));
         mapa.adicionarItem(pontoDesembarque);
 
         Localizacao ponto1 = pontoEmbarque.getLocalizacaoAtual();
         Localizacao ponto2 = pontoDesembarque.getLocalizacaoAtual();
-
 
         Localizacao posInicial = new Localizacao(ponto1.getX() + 1, ponto1.getY() - 1);
         Localizacao posFinal = new Localizacao(ponto2.getX() + 1, ponto2.getY() - 1);
