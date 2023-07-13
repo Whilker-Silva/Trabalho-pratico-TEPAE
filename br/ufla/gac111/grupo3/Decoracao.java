e5f91b905feb5d276c86cabd3f7a666c144de794
Victor Hugo Daia Lorenzato <113929927+Victor-Lorenzato@users.noreply.github.com>
2023-07-13 12:08:09 -0300

    Decoracao-comentada

--- br/ufla/gac111/grupo3/Decoracao.java
+++ br/ufla/gac111/grupo3/Decoracao.java
@@ -2,8 +2,24 @@ package br.ufla.gac111.grupo3;
 
 import javax.swing.ImageIcon;
 
+/**
+ * Classe responsável por aparatar o mapa de simulação
+ * adicionando imagens no mapa de simulação.
+ */
 public class Decoracao extends Item {
-    public Decoracao(Localizacao localizacao, String imagem){
+
+    /**
+     * Construtor de objetos do tipo Decoracao
+     * <p>
+     * A imagem é carregada ImageIcon e o método getResource() para carregar a
+     * imagem
+     * Método getImage() utilizado para obter a imagem e defini-la na decoração.
+     * 
+     * @param localizacao
+     * @param imagem
+     */
+
+    public Decoracao(Localizacao localizacao, String imagem) {
         super(localizacao);
         setImagem(new ImageIcon(getClass().getResource(imagem)).getImage());
     }
